d68f95ae9d6eb4eb78bd087216a62e1683e52f34
Orion <orionserup@gmail.com>
2022-03-08 18:15:58 -0800

    update

--- src/TesterBackend.py
+++ src/TesterBackend.py
@@ -26,7 +26,7 @@ class CatheterTester:
         if self.vna is None:
             return
 
-        self.vna.SetFileName(filename)
+        self.vna.SetFileName(filename + str(self.channel + 1))
         self.vna.Sweep()
 
     def PulseEchoTest(self, channel: int = 1, duration_us: float = 6.0, filename: str = "cath.csv") -> bool:
@@ -36,12 +36,14 @@ class CatheterTester:
         
         self.scope.CaptureWaveform(channel, duration_us)
         self.scope.CalculateFFT()
-        self.scope.WriteDataToFile(filename)
+        self.scope.WriteDataToFile(filename + str(self.channel + 1))
 
     def SetChannel(self, channel: int):
 
         if(channel < 0):
             return
+            
+        self.channel = channel % max_channel
 
         relay_ch = (channel & 0x30) >> 4 # get the two bit relay channel 
         channel &= ~0x30 # clear the bits of the relay channel
@@ -59,7 +61,7 @@ class CatheterTester:
         channel |= relay_ch << 4 # put the correct index in
 
         self.arduino.Write(channel.to_bytes(1, 'big'))
-
+        
 
 
 if __name__ == "__main__":
