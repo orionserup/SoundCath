f721a8f309e84821c8ccfb5b8b1a029084f5d64b
Orion Serup <48197072+orionserup@users.noreply.github.com>
2022-07-21 14:02:32 -0700

    fixed interdocument copy

--- src/SoundCath.py
+++ src/SoundCath.py
@@ -7,6 +7,7 @@ import os
 import threading
 import openpyxl
 from openpyxl.styles import fills, colors
+import copy from copy
 
 vnachanneloffset = 1 << 7
 scopechanneloffset = 1 << 6
@@ -194,12 +195,12 @@ class TesterFrontEnd:  # a GUI front end for the test
         impedancereporttemplate = openpyxl.load_workbook(filename = templatepath + "ImpedanceTemplate.xlsx")
         pereporttemplate = openpyxl.load_workbook(filename = templatepath + "PETemplate.xlsx")
         
-        donglereport = report.copy_worksheet(donglereporttemplate.active)
-        donglereport.title = "Dongle"
-        impedancereport = report.copy_worksheet(impedancereporttemplate.active)
-        impedancereport.title = "Impedance"
-        pereport = report.copy_worksheet(pereporttemplate.active)
-        pereport.title = "PulseEcho"
+        donglereport = report.create_sheet("Dongle")
+        copy_sheet(donglereporttemplate.active, donglereport)
+        impedancereport = report.create_sheet("Impedance")
+        copy_sheet(impedancereporttemplate.active, impedancereport)
+        pereport = report.create_sheet("Pulse Echo")
+        copy_sheet(pereporttemplate.active, pereport)
         
         red = colors.Color("00FF0000")
         green = colors.Color("0000FF00")
@@ -245,7 +246,60 @@ class TesterFrontEnd:  # a GUI front end for the test
                 
         filename = os.getcwd() + '\\' + self.filename.get() + "Report.xlsx"
         report.save(filename)
-              
+
+
+def copy_sheet(source_sheet, target_sheet):
+    copy_cells(source_sheet, target_sheet)  # copy all the cel values and styles
+    copy_sheet_attributes(source_sheet, target_sheet)
+
+
+def copy_sheet_attributes(source_sheet, target_sheet):
+    target_sheet.sheet_format = copy(source_sheet.sheet_format)
+    target_sheet.sheet_properties = copy(source_sheet.sheet_properties)
+    target_sheet.merged_cells = copy(source_sheet.merged_cells)
+    target_sheet.page_margins = copy(source_sheet.page_margins)
+    target_sheet.freeze_panes = copy(source_sheet.freeze_panes)
+
+    # set row dimensions
+    # So you cannot copy the row_dimensions attribute. Does not work (because of meta data in the attribute I think). So we copy every row's row_dimensions. That seems to work.
+    for rn in range(len(source_sheet.row_dimensions)):
+        target_sheet.row_dimensions[rn] = copy(source_sheet.row_dimensions[rn])
+
+    if source_sheet.sheet_format.defaultColWidth is None:
+        print('Unable to copy default column wide')
+    else:
+        target_sheet.sheet_format.defaultColWidth = copy(source_sheet.sheet_format.defaultColWidth)
+
+    # set specific column width and hidden property
+    # we cannot copy the entire column_dimensions attribute so we copy selected attributes
+    for key, value in source_sheet.column_dimensions.items():
+        target_sheet.column_dimensions[key].min = copy(source_sheet.column_dimensions[key].min)   # Excel actually groups multiple columns under 1 key. Use the min max attribute to also group the columns in the targetSheet
+        target_sheet.column_dimensions[key].max = copy(source_sheet.column_dimensions[key].max)  # https://stackoverflow.com/questions/36417278/openpyxl-can-not-read-consecutive-hidden-columns discussed the issue. Note that this is also the case for the width, not onl;y the hidden property
+        target_sheet.column_dimensions[key].width = copy(source_sheet.column_dimensions[key].width) # set width for every column
+        target_sheet.column_dimensions[key].hidden = copy(source_sheet.column_dimensions[key].hidden)
+
+
+def copy_cells(source_sheet, target_sheet):
+    for (row, col), source_cell in source_sheet._cells.items():
+        target_cell = target_sheet.cell(column=col, row=row)
+
+        target_cell._value = source_cell._value
+        target_cell.data_type = source_cell.data_type
+
+        if source_cell.has_style:
+            target_cell.font = copy(source_cell.font)
+            target_cell.border = copy(source_cell.border)
+            target_cell.fill = copy(source_cell.fill)
+            target_cell.number_format = copy(source_cell.number_format)
+            target_cell.protection = copy(source_cell.protection)
+            target_cell.alignment = copy(source_cell.alignment)
+
+        if source_cell.hyperlink:
+            target_cell._hyperlink = copy(source_cell.hyperlink)
+
+        if source_cell.comment:
+            target_cell.comment = copy(source_cell.comment)
+            
 
 if __name__ == "__main__":
 
