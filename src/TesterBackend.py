7ac8c46457286b98053948153afb6648c4d321a9
jlop2 <99764964+jlop2@users.noreply.github.com>
2022-11-04 14:34:49 -0700

    Update TesterBackend.py

--- src/TesterBackend.py
+++ src/TesterBackend.py
@@ -25,9 +25,9 @@ channel_lower_thresh = 750e-12
 channel_freq = 800e3
 
 # pulse echo when to start the waveform capture
-scope_window_start_us = 49.4
+scope_window_start_us = 49.6
 # pulse echo how wide of a window to examine
-scope_window_width_us = 1.1
+scope_window_width_us = .8
 
 # pulse echo fft when to start the window
 fft_window_start = 2e6
